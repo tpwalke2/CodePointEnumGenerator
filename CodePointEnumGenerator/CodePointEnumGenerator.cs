9c6080362ea3b27016af28afb24578e3408e6769
Tom Walker <tpwalke2@gmail.com>
2022-12-24 12:31:05 -0500

    Refactor to separate responsibilities across multiple files

--- CodePointEnumGenerator/CodePointEnumGenerator.cs
+++ CodePointEnumGenerator/CodePointEnumGenerator.cs
@@ -12,6 +12,8 @@ namespace CodePointEnumGenerator;
 [Generator]
 public class CodePointEnumGenerator : ISourceGenerator
 {
+    private const string CodepointsExtension = ".codepoints";
+
     public void Initialize(GeneratorInitializationContext context)
     {
         // initialization not required
@@ -34,70 +36,22 @@ public class CodePointEnumGenerator : ISourceGenerator
         
         foreach (var file in files)
         {
-            context.ReportDiagnostic(
-                Diagnostic.Create(
-                    new DiagnosticDescriptor(
-                        "GENERATING",
-                        $"Generating enum for {file.Path}",
-                        "",
-                        "",
-                        DiagnosticSeverity.Info,
-                        true),
-                    null));
-            var (enumName, contents) = GenerateEnumFile(file);
-            context.AddSource($"{enumName}.g.cs", SourceText.From(contents, Encoding.UTF8));
+            var enumName = file.GenerateEnumName();
+
+            context.AddSource(
+                $"{enumName}.g.cs",
+                SourceText.From(
+                    CodeGeneration.BuildEnumFileContents(
+                        enumName,
+                        file.GetEnumValues(),
+                        file.Path.ToNamespace()),
+                    Encoding.UTF8));
         }
     }
 
     private static IEnumerable<AdditionalText> GetCodePointFiles(GeneratorExecutionContext context) =>
         context.AdditionalFiles.Where(file => Path.GetExtension(file.Path)
-                                                  .Equals(".codepoints", StringComparison.OrdinalIgnoreCase));
-
-    private static (string enumName, string contents) GenerateEnumFile(AdditionalText file)
-    {
-        var enumName = Path.GetFileName(file.Path)
-                           .Replace(".codepoints", "", StringComparison.OrdinalIgnoreCase)
-                           .Replace("-", "");
-        
-        var seenEnums = new Dictionary<string, int>();
-        var values    = new List<(string, string)>();
-
-        foreach (var line in file.GetText()!
-                                 .ToString()
-                                 .Split(
-                                     new[] { "\r\n", "\r", "\n" },
-                                     StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
-        {
-            var parsed = line.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-            if (parsed.Length != 2) continue;
-
-            var enumValueName = parsed[0].ToEnumEntry();
-            if (!seenEnums.ContainsKey(enumValueName))
-            {
-                seenEnums[enumValueName] = 1;
-            }
-            else
-            {
-                seenEnums[enumValueName]++;
-                enumValueName = $"{enumValueName}{seenEnums[enumValueName]}";
-            }
-
-            values.Add((enumValueName, parsed[1]));
-        }
-
-        var sb = new StringBuilder();
-
-        sb.Append($@"
-namespace {file.Path.ToNamespace()};
-
-public enum {enumName} {{
-");
-
-        sb.AppendJoin(",\n", values.Select(tuple => $@"    {tuple.Item1} = 0x{tuple.Item2}"));
-
-        sb.Append(@"
-}");
-        
-        return (enumName, sb.ToString());
-    }
+                                                  .Equals(
+                                                      CodepointsExtension,
+                                                      StringComparison.OrdinalIgnoreCase));
 }
---
+++ CodePointEnumGenerator/Helpers/CodeGeneration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodePointEnumGenerator.Helpers;
+
+public static class CodeGeneration
+{
+    public static string BuildEnumFileContents(
+        string enumName,
+        IEnumerable<(string, string)> values,
+        string @namespace) => new StringBuilder()
+                              .Append($@"
+namespace {@namespace};
+
+public enum {enumName} {{
+")
+                              .AppendJoin(",\n", values.Select(tuple => $@"    {tuple.Item1} = 0x{tuple.Item2}"))
+                              .Append(@"
+}")
+                              .ToString();
+}
