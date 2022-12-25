1210325a78a70491c4790f3683117dd35a6d1d70
Tom Walker <tpwalke2@gmail.com>
2022-12-25 05:14:19 -0500

    Move test class to separate file

---
+++ CodePointEnumGenerator.Tests/AdditionalFile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+using System.Threading;
+
+namespace CodePointEnumGenerator.Tests;
+
+public sealed class AdditionalFile : AdditionalText
+{
+    private readonly SourceText _sourceText;
+        
+    public AdditionalFile(string path, string contents)
+    {
+        Path        = path;
+        _sourceText = SourceText.From(contents, Encoding.UTF8);
+    }
+
+    public override SourceText? GetText(CancellationToken cancellationToken = new()) =>
+        _sourceText;
+
+    public override string Path { get; }
+}
--- CodePointEnumGenerator.Tests/GeneratorTestFactory.cs
+++ CodePointEnumGenerator.Tests/GeneratorTestFactory.cs
@@ -72,20 +72,4 @@ public static class GeneratorTestFactory
                      .Select(d => d.Id.ToString())
                      .All(IgnoredPreDiagnosticErrors.Contains);
     }
-
-    private sealed class AdditionalFile : AdditionalText
-    {
-        private readonly SourceText _sourceText;
-        
-        public AdditionalFile(string path, string contents)
-        {
-            Path = path;
-            _sourceText = SourceText.From(contents, Encoding.UTF8);
-        }
-
-        public override SourceText? GetText(CancellationToken cancellationToken = new()) =>
-            _sourceText;
-
-        public override string Path { get; }
-    }
 }
