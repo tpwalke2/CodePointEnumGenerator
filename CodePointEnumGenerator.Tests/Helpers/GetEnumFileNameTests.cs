f1623a257db8ca2717d47d42cae34c7872cf6661
Tom Walker <tpwalke2@gmail.com>
2022-12-25 05:14:54 -0500

    Add tests for getting enum file name from codepoints path

---
+++ CodePointEnumGenerator.Tests/Helpers/GetEnumFileNameTests.cs
@@ -0,0 +1,18 @@
+﻿using CodePointEnumGenerator.Helpers;
+using Xunit;
+
+namespace CodePointEnumGenerator.Tests.Helpers;
+
+public class GetEnumFileNameTests
+{
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("Arial.codepoints", "Arial")]
+    [InlineData("MaterialIcons-Regular.codepoints", "MaterialIconsRegular")]
+    public void GetEnumFileName(string filePath, string expectedFilename)
+    {
+        var file = new AdditionalFile(filePath, "");
+        
+        Assert.Equal(expectedFilename, file.GetEnumFileName());
+    }
+}
