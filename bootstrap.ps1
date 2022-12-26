4b2a1d52815880c457c7ad440eb4a2ea1fae78b9
Tom Walker <tpwalke2@gmail.com>
2022-12-26 06:17:37 -0500

    #3 invalid parameter passing to cake

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,11 +1,8 @@
 param (
-    [Parameter(Mandatory=$true)]
     [int]$buildNumber,
-    [Parameter(Mandatory=$true)]
     [System.String]$branch,
-    [Parameter(Mandatory=$true)]
     [System.String]$buildPath)
 
 dotnet tool restore
-dotnet cake build.cake --bootstrap --buildNumber=$buildNumber -branch=$branch -buildPath="$buildPath"
-dotnet cake build.cake --buildNumber=$buildNumber -branch=$branch -buildPath="$buildPath"
+dotnet cake build.cake --bootstrap --buildNumber=$buildNumber --branch="$branch" --buildPath="$buildPath"
+dotnet cake build.cake --buildNumber=$buildNumber --branch="$branch" --buildPath="$buildPath"
