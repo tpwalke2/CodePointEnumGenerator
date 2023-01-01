03e416e8a14fb6ce168caa6da295ff31d2d4e77c
Tom Walker <tpwalke2@gmail.com>
2023-01-01 05:23:31 -0500

    #4 tag name is only in environment variable if the build is from a tag

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,7 +4,11 @@ param (
     [System.String]$buildPath,
     [System.String]$gitHubApiKey,
     [System.String]$nuGetApiKey,
-    [System.String]$tagName)
+    [bool]$isTag,
+    [System.String]$tagVariableName)
+
+$tagName = ""
+if ($isTag) { $tagName = [System.Environment]::GetEnvironmentVariable($tagVariableName) }
 
 dotnet tool restore
 dotnet cake build.cake --bootstrap --buildNumber=$buildNumber --branch="$branch" --buildPath="$buildPath" --gitHubApiKey="$gitHubApiKey" --nuGetApiKey="$nuGetApiKey" --tagName="$tagName"
